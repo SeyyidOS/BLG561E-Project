5fd4f9c7a2b49bc82541feb8f194298cc6abdc6d
SeyyidOS <seyyidosmansevgili@gmail.com>
2025-01-03 21:10:13 +0000

    Data cleaning code added

--- src/data_helper.py
+++ src/data_helper.py
@@ -1,7 +1,22 @@
 import pandas as pd
+import re
 
 
-def generate_player_data(data: dict) -> pd.DataFrame:
+def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+    data = df.copy()
+    data["height"] = data["height"].str.replace(" cm", "", regex=False).astype(float)
+    data["weight"] = data["weight"].str.replace(" kg", "", regex=False).astype(float)
+
+    data = pd.get_dummies(data, columns=["nationality", "position"], prefix=["nationality", "position"])
+
+    year_columns = [col for col in data.columns if re.match(r'^\d{4}(-\d+)?$', col)]
+    data = pd.get_dummies(data, columns=year_columns, prefix=year_columns)
+
+    data = data.drop(columns=["photo", "player_name"])
+    
+    return data
+
+def generate_player_data(data: dict) -> tuple[pd.DataFrame, dict]:
     player_rows = {}
     team_id_to_name = {}
 
@@ -36,7 +51,7 @@ def generate_player_data(data: dict) -> pd.DataFrame:
                     season_tracker[season] = 1
                     season_key = str(season)
 
-                row[season_key] = team_id
+                row[season_key] = team
 
         player_rows[player_name] = row
 
